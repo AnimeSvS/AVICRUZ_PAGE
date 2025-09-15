b1859f82f5efecd2cd124e349fdff881e71c8e3f
AnimeSvS <andysevas2@gmail.com>
2025-09-14 22:58:41 -0500

    v3

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtEcRPDDBUhvROqh8jLXa6a2MGOb_046E",
+  authDomain: "dbavicruz.firebaseapp.com",
+  databaseURL: "https://dbavicruz-default-rtdb.firebaseio.com",
+  projectId: "dbavicruz",
+  storageBucket: "dbavicruz.firebasestorage.app",
+  messagingSenderId: "208341540001",
+  appId: "1:208341540001:web:f31a24ffe40450a401e4e0",
+  measurementId: "G-FNP46FB729"
+};
+firebase.initializeApp(firebaseConfig);
